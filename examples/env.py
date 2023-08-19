7f8f24507b59ddcf513a5822493a163619e800b8
muchvo <borongzh@gmail.com>
2023-08-19 18:47:13 +0800

    x

--- examples/env.py
+++ examples/env.py
@@ -15,35 +15,62 @@
 """Examples for environments."""
 
 import argparse
+import os
+
+import glfw
+import mujoco
+import numpy as np
+from gymnasium.utils.save_video import save_video
 
 import safety_gymnasium
 
 
+DIR = os.path.join(os.path.dirname(__file__), 'cached_test_vision_video')
+
+
 def run_random(env_name):
     """Random run."""
-    env = safety_gymnasium.make(env_name, render_mode='human')
+    env = safety_gymnasium.make(env_name)
     obs, info = env.reset()  # pylint: disable=unused-variable
     # Use below to specify seed.
     # obs, _ = env.reset(seed=0)
     terminated, truncated = False, False
     ep_ret, ep_cost = 0, 0
-    while True:
-        if terminated or truncated:
+    render_list = []
+    for i in range(1001):
+        if i == 1000:
             print(f'Episode Return: {ep_ret} \t Episode Cost: {ep_cost}')
             ep_ret, ep_cost = 0, 0
             obs, info = env.reset()  # pylint: disable=unused-variable
+            save_video(
+                frames=render_list,
+                video_folder=DIR,
+                name_prefix='test_vision_output',
+                fps=30,
+            )
+            render_list = []
         assert env.observation_space.contains(obs)
         act = env.action_space.sample()
         assert env.action_space.contains(act)
         # pylint: disable-next=unused-variable
         obs, reward, cost, terminated, truncated, info = env.step(act)
-
+        env.task.render(mode='human', width=1280, height=1280, cost={'cost_sum': cost})
+        img = np.zeros(
+            (
+                glfw.get_framebuffer_size(env.task.viewer.window)[1],
+                glfw.get_framebuffer_size(env.task.viewer.window)[0],
+                3,
+            ),
+            dtype=np.uint8,
+        )
+        mujoco.mjr_readPixels(img, None, env.task.viewer.viewport, env.task.viewer.con)
+        render_list.append(np.flipud(img))
         ep_ret += reward
         ep_cost += cost
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', default='SafetyPointFormulaOne2Debug-v0')
+    parser.add_argument('--env', default='SafetyRacecarFormulaOne2Debug-v0')
     args = parser.parse_args()
     run_random(args.env)
